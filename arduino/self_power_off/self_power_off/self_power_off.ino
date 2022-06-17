aa08b803cc19c6c9eafc8aadba01d351fe8ddd82
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2022-06-17 09:03:22 +0100

    Improve self power arduino code

--- arduino/self_power_off/self_power_off/self_power_off.ino
+++ arduino/self_power_off/self_power_off/self_power_off.ino
@@ -1,11 +1,16 @@
-const int POWERPIN = 7;
-
+const int POWERPIN = 9;
+const int LEDPIN = 8;
 void setup() {
   pinMode(POWERPIN, OUTPUT);
   digitalWrite(POWERPIN, HIGH);
+  pinMode(LEDPIN, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(LEDPIN, HIGH);
+  delay(1000);
+  digitalWrite(LEDPIN, LOW);
+  delay(1000);
   delay(5000);
   digitalWrite(POWERPIN, LOW);
 }
